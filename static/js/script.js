85ef00d54a0f99236fa543e91b4f1874bd77a663
Sonia Majewska <sonianltran@gmail.com>
2024-05-05 22:13:28 +0000

    Add css.styling to base.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('#textarea1').val('New Text');
+    M.textareaAutoResize($('#textarea1'));
     $('#category_color_input').on('input', function() {
             // Get the new color value from the input field
             var newColor = $(this).val();
